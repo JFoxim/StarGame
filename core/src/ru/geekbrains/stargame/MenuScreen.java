ddb60bbd7763b2991175c701d9a56c0c6f7c58b1
ShangareevRR <renanew@mail.ru>
2018-01-28 22:53:47 +0500

    Добавлено ДЗ 2. Перерисовка корабля в позиции клика мышки или тачскрина.

---
+++ core/src/ru/geekbrains/stargame/MenuScreen.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.stargame;
+
+/**
+ * Created by User on 28.01.2018.
+ */
+
+public class MenuScreen {
+}
---
+++ engine/src/ru/geekbrains/stargame/engine/Base2DScreen.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.stargame.engine;
+
+/**
+ * Created by User on 28.01.2018.
+ */
+
+public class Base2DScreen {
+}
